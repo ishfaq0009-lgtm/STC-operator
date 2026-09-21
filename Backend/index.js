c37e97236ae9c48f85e435832b611f3430bd88d3
ishfaq0009-lgtm <ishfaqpstore@gmail.com>
2026-09-21 12:12:26 -0700

    backend index.j fils change

--- Backend/index.js
+++ Backend/index.js
@@ -1,36 +1,34 @@
-import express from "express";
-import cors from "cors";
-import dbconection from "./db/dbconection.js";
-import userRouter from "./Routes/user.route.js";
-import ProjectRouter from "./Routes/Project.route.js"
-import cookieparser from "cookie-parser";
-import dotenv from "dotenv"
-import MemberRouter from "./Routes/Member.route.js"
+  import express from "express";
+  import cors from "cors";
+  import dbconection from "./db/dbconection.js";
+  import userRouter from "./Routes/user.route.js";
+  import ProjectRouter from "./Routes/Project.route.js"
+  import cookieparser from "cookie-parser";
+  import dotenv from "dotenv"
+  import MemberRouter from "./Routes/Member.route.js"
 
- dbconection();
-
- dotenv.config()
-
-let app = express();
-
-app.use(express.json());
-app.use(cookieparser());
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+  dotenv.config();
+  dbconection();
 
 
-app.use("/user", userRouter);
-app.use("/Project",ProjectRouter);
-app.use("/members",MemberRouter);
+  let app = express();
 
-app.use("/",async function(req,res){
-  
-  return res.json({success:true,msg:"this is backend working."});
-});
+  app.use(express.json());
+  app.use(cookieparser());
+  app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+  }));
 
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
-});
+  app.use("/user", userRouter);
+  app.use("/Project",ProjectRouter);
+  app.use("/members",MemberRouter);
+
+  app.use("/",async function(req,res){
+    
+    return res.json({success:true,msg:"this is backend working."});
+  });
+
+
+  export default app;
