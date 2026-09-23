011c3627b92a516cd30dc1990a2703b41ab059fb
ishfaq0009-lgtm <ishfaqpstore@gmail.com>
2026-09-23 01:09:05 -0700

    Registration.js change

--- Frontend/src/Pages/Registration.jsx
+++ Frontend/src/Pages/Registration.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+
 
 const Registration = () => {
 
